57d01f02689413669e53c88df92b9df36c898360
Andre Rodrigues <andre.msr@icloud.com>
2018-12-15 18:43:52 -0200

    Add tests for track loading

--- DogsChallengeTests/Extensions/RxExtensionsTests.swift
+++ DogsChallengeTests/Extensions/RxExtensionsTests.swift
@@ -1,6 +1,7 @@
 import Nimble
 import RxBlocking
 import RxSwift
+import RxCocoa
 import XCTest
 
 @testable import DogsChallenge
@@ -38,5 +39,28 @@ final class ObservableHTTPTests: XCTestCase {
 
         expect(try observable.mapDecodable(TestModel.self).toBlocking().first())
             .to(equal(TestModel(value: value)))
+    }
+
+    func testTrackLoading() {
+        struct Error: Swift.Error {}
+        class Loader {
+            var loading: Bool = false
+        }
+        let loader = Loader()
+        let binder = Binder<Bool>(loader) {
+            $0.loading = $1
+        }
+
+        let subject = PublishSubject<Void>()
+
+        _ = subject.take(1).asSingle().trackLoading(binder: binder).subscribe()
+        expect(loader.loading).toEventually(beTrue())
+        subject.onNext(())
+        expect(loader.loading).toEventually(beFalse())
+
+        _ = subject.take(1).asSingle().trackLoading(binder: binder).subscribe()
+        expect(loader.loading).toEventually(beTrue())
+        subject.onError(Error())
+        expect(loader.loading).toEventually(beFalse())
     }
 }
