9fdf9ae2dab09ed0b389a89d35fefcdcce4e43f9
Andre Rodrigues <andre.msr@icloud.com>
2018-12-15 17:08:39 -0200

    Add scheduler to http get gateway

--- DogsChallenge/Core/Network/HttpGetGateway.swift
+++ DogsChallenge/Core/Network/HttpGetGateway.swift
@@ -3,13 +3,15 @@ import RxSwift
 class HttpGetGateway<Model: Decodable>: GetGateway {
     private let client: HttpClient
     private let endpoint: URL
+    private let scheduler: SchedulerType
 
-    init(client: HttpClient, endpoint: URL) {
+    init(client: HttpClient, endpoint: URL, scheduler: SchedulerType = MainScheduler.instance) {
         self.client = client
         self.endpoint = endpoint
+        self.scheduler = scheduler
     }
 
     func get() -> Single<Model> {
-        return client.get(endpoint: endpoint)
+        return client.get(endpoint: endpoint).observeOn(scheduler)
     }
 }
