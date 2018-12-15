f827a38f612fba690aa046bc625b41af6d3f5336
Andre Rodrigues <andre.msr@icloud.com>
2018-12-15 17:49:01 -0200

    Add empty init in base view controller

--- DogsChallenge/Core/UI/ViewController.swift
+++ DogsChallenge/Core/UI/ViewController.swift
@@ -1,6 +1,14 @@
 import UIKit
 
 class ViewController: UIViewController {
+    init() {
+        super.init(nibName: nil, bundle: nil)
+    }
+
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+    }
+
     override func loadView() {
         super.loadView()
         initialize()
