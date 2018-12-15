f9acf059193959fb9045b35c47d394f4574cf8ae
Andre Rodrigues <andre.msr@icloud.com>
2018-12-15 18:26:56 -0200

    Add breeds view controller on app startup

--- DogsChallenge/Application/AppDelegate.swift
+++ DogsChallenge/Application/AppDelegate.swift
@@ -7,9 +7,14 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
 
         let window = UIWindow(frame: UIScreen.main.bounds)
-        let viewController = UIViewController()
-
+        
+        #if !TEST
+        let viewController = BreedsViewController()
         window.rootViewController = viewController
+        #else
+        window.rootViewController = UIViewController()
+        #endif
+
         window.makeKeyAndVisible()
         self.window = window
 
